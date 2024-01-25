daae3db2ae30a39dbcc800331168b67883194250
Jonah Ryan <87236765+jonahrobot@users.noreply.github.com>
2024-01-25 10:01:08 -0800

    HW 1

---
+++ printName.py
@@ -0,0 +1,6 @@
+# Print Bio
+
+if __name__ == "__main__":
+    print("Jonah Ryan (He / Him).\n")
+    print("Favorite movie is Speed! It is a action aged to be a comedy\n")
+    print("Favorite food... Tiramisu\n")
